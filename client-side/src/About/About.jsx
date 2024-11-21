6397336ccf11e0a56e518b6ebb48e9d441362cdd
ruu23 <arwamah777@gmail.com>
2024-11-21 22:34:52 +0200

    update in about file

--- client-side/src/About/About.jsx
+++ client-side/src/About/About.jsx
@@ -7,8 +7,57 @@ const About = () => {
     <>
       <HeaderAndSideBar />
       <section className="about">
-        {/* About section content */}
-      </section>
+  <div className="row">
+    <div className="image">
+      <img src="images/about-img.svg" alt="" />
+    </div>
+
+    <div className="content">
+      <h3>Why Choose Us?</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut dolorum
+        quasi illo? Distinctio expedita commodi, nemo a quam error repellendus
+        sint, fugiat quis numquam eum eveniet sequi aspernatur quaerat tenetur.
+      </p>
+      <a href="courses.html" className="inline-btn">Our Courses</a>
+    </div>
+  </div>
+
+  <div className="box-container">
+    <div className="box">
+      <i className="fas fa-graduation-cap"></i>
+      <div>
+        <h3>+10k</h3>
+        <p>Online Courses</p>
+      </div>
+    </div>
+
+    <div className="box">
+      <i className="fas fa-user-graduate"></i>
+      <div>
+        <h3>+40k</h3>
+        <p>Brilliant Students</p>
+      </div>
+    </div>
+
+    <div className="box">
+      <i className="fas fa-chalkboard-user"></i>
+      <div>
+        <h3>+2k</h3>
+        <p>Expert Tutors</p>
+      </div>
+    </div>
+
+    <div className="box">
+      <i className="fas fa-briefcase"></i>
+      <div>
+        <h3>100%</h3>
+        <p>Job Placement</p>
+      </div>
+    </div>
+  </div>
+</section>
+
 
       <section className="reviews">
         <h1 className="heading">Student's Reviews</h1>

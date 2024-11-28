14d353e4da7cd9f8deabfba08ef21aea9c81a170
Mano224 <mano@example.com>
2024-11-28 20:26:43 +0200

    update for videos

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
-from flask import request,jsonify
-from config import app,db
-from models import Users
+from flask import request, jsonify
+from config import create_app, db
+from models import Users, Playlist, Video, init_db
+from werkzeug.utils import secure_filename
+import os
+
+app = create_app()
 
 
 # get-user
@@ -14,11 +18,11 @@ def get_user():
 @app.route('/register',methods=["POST"])
 def register():
     data = request.get_json()
-    username = data.get("username")
+    username = data.get("name")
     email = data.get("email")
-    password = data.get("password")
-    confirm_password = data.get("confirm_password")
-    img_url = data.get("select_Profile")
+    password = data.get("pass")
+    confirm_password = data.get("c_pass")
+    img_url = request.files['img_url']
 
     if not username or not email or not password or not confirm_password:
         return jsonify({'error': 'All fields are required'}), 400
@@ -29,27 +33,32 @@ def register():
     if Users.query.filter_by(username=username).first() or Users.query.filter_by(email=email).first():
         return jsonify({'error': 'Username or email already exists'}), 409
     
+    if img_url:
+        filename = secure_filename(img_url.filename)  
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{username}_{filename}")
+        img_url.save(file_path)
     # Create and save the new user
     user = Users(username=username, email=email, img_url=img_url)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({'message': 'User registered successfully'}), 201
+    return jsonify({'message': 'User registered successfully', 'img_url': file_path}), 201
 
 
 # login
 @app.route('/login',methods=["POST"])
 def login():
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('passwird') 
+    email = data.get('email')
+    password = data.get('pass') 
     
-    if not username or not password:
+    if not email or not password:
         return jsonify({'error': 'Username and password are required'}), 400
     
-    user = Users.query.filter_by(username=username).first()
-    assert user , "user not exist"
+    user = Users.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"message": "user Not exsit"})
 
     # check auth
     if user and user.check_password(password):
@@ -57,7 +66,70 @@ def login():
         return jsonify({'message': f'Welcome, {user.username}!'}), 200
     else:
         # Authentication failed
-        return jsonify({'error': 'Invalid username or password'}), 401
+        return jsonify({'message': 'Invalid email or password'}), 401
+
+
+# update-profile
+@app.route('/update-profile/<int:id>', methods=['PATCH'])
+def update_profile(id):
+    try:
+        user = Users.query.get(id)
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        
+        data = request.json
+        user.username = data.get("username")
+        user.email = data.get("email")
+        old_password = data.get("old_password")
+        new_password = data.get("new_password")
+        confirm_password = data.get("confirm_password")
+
+        if old_password:
+            if user.check_password(old_password):
+                return jsonify({'error': 'Old password is incorrect'}), 400
+            if new_password != confirm_password:
+                return jsonify({'error': 'New password and confirm password do not match'}), 400
+            user.set_password(old_password)
+
+
+        if 'img_url' in request.files:
+            img_url = request.files['img_url']
+        if img_url:
+            filename = secure_filename(img_url.filename) 
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"user_{id}_{filename}")
+            img_url.save(file_path)
+            user.img_url = file_path
+
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/courses', methods=["GET"])
+def courses():
+    Playlists = Playlist.query.all()
+    return jsonify([{
+        "id" : p.id,
+        "title": p.title,
+    } for p in Playlists])
+
+@app.route('/courses/<int:playlist_id>', methods=["GET"])
+def videos(playlist_id):
+    plaulist = Playlist.query.get(playlist_id)
+
+    if not plaulist:
+        return jsonify({"message" : " No playlist found"})
+    
+    videos = Video.query.filter_by(playlist_id=playlist_id).all()
+    return jsonify([{
+        "id" : v.id,
+        "title" : v.title,
+        "description" : v.description,
+        "thumbnail" : v.thumbnail,
+        "video_url": v.video_url
+    } for v in videos])
+
+
 
 
 
@@ -69,4 +141,6 @@ def login():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        init_db()
+
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,9 @@
 from config import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class Users(db.Model):
-    id = db.Column("id", db.Integer, primarykey=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), unique=True, nullable=False)
@@ -21,3 +22,67 @@ class Users(db.Model):
             "imgUrl":self.img_url,
 
         }
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    video_url = db.Column(db.String(200), nullable=False)
+    thumbnail = db.Column(db.String(200), nullable=False)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'), nullable=False)
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    Videos = db.relationship('Video', backref='playlist', lazy=True)
+
+
+
+def init_db():
+    playlist = Playlist(title= "Complete HTML Tutorial")
+    video1 = Video(
+                title = "Complete HTML Tutorial (Part 01)",
+                description = "Introduction and What I Need To Learn",
+                video_url = "static/videos/Html_v1.mp4",
+                thumbnail = "static/imgs/html-1.png",
+                playlist=playlist
+    )
+    video2 = Video(
+                title = "Complete HTML Tutorial (Part 02)",
+                description = "Elements And Browser",
+                video_url = "static/videos/Html_v1.mp4", # missed
+                thumbnail = "static/imgs/html-2.png",
+                playlist=playlist
+    )
+    video3 = Video(
+                title = "Complete HTML Tutorial (Part 03)",
+                description = "First Project And First Page",
+                video_url = "static/videos/Html_v1.mp4", # missed
+                thumbnail = "static/imgs/html-3.png",
+                playlist=playlist
+    )
+    video4 = Video(
+                title = "Complete HTML Tutorial (Part 04)",
+                description = "Head And Nested Elements",
+                video_url = "static/videos/Html_v1.mp4", # missed
+                thumbnail = "static/imgs/html-4.png",
+                playlist=playlist
+    )
+    video5 = Video(
+                title = "Complete HTML Tutorial (Part 05)",
+                description = "Comments And Use Cases",
+                video_url = "static/videos/Html_v1.mp4", # missed
+                thumbnail = "static/imgs/html-5.png",
+                playlist=playlist
+    )
+    video6 = Video(
+                title = "Complete HTML Tutorial (Part 06)",
+                description = "Doctype And Standard And Quirks Mode",
+                video_url = "static/videos/Html_v1.mp4", # missed
+                thumbnail = "static/imgs/html-6.png",
+                playlist=playlist
+    )
+
+    db.session.add(playlist)
+    db.session.add_all([video1, video2, video3, video4, video5, video6])
+    db.session.commit()
